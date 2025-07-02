faedfd587b439a71d5e5371f8204ba5115b2461a
Andrew <pulsipher.a@gmail.com>
2025-07-02 09:53:10 -0600

    Create filesystem.R with create_filesystem and read_report_config functions

---
+++ R/filesystem.R
@@ -0,0 +1,68 @@
+#' Create filesystem
+#'
+#' `create_filesystem` creates the given folders if they don't already exist.
+#'
+#' @param internal Filepath. Folder to hold internal reports.
+#' @param public Filepath. Folder to hold public reports.
+#' @param settings Filepath. Folder to hold report settings.
+#'
+#' @returns NULL.
+create_filesystem <- function(internal, public, settings) {
+  # - Create folders if needed
+  for (f in c(internal, public, settings)) {
+    if (!dir.exists(f)) {
+      dir.create(f)
+    }
+  }
+}
+
+
+#' Read in the report config YAML file
+#'
+#' 'read_report_config' reads in the config YAML file
+#'
+#' @param config_filepath Filepath. Path to report config file.
+#'
+#' @returns a named list with an attribute of 'keys' from the file.
+read_report_config <- function(config_filepath) {
+
+  if (file.exists(config_filepath)) {
+    config <- read_yaml(config_filepath)
+
+    # - Validate or set defaults
+    if (is.null(config$current_population) ||
+        class(config$current_population) != "integer") {
+      warning("In '", config_filepath, "', 'current_population' is missing or
+            invalid. Using default value of 100,000 instead.")
+      config$current_population <- 100000
+    }
+
+    if (is.null(config$avg_5yr_population) ||
+        class(config$avg_5yr_population) != "integer") {
+      warning("In '", config_filepath, "', 'avg_5yr_population' is missing or
+            invalid. Using default value of 'current_population' instead.")
+      config$avg_5yr_population <- config$current_population
+    }
+
+    if (is.null(config$rounding_decimals) ||
+        class(config$rounding_decimals) != "integer") {
+      warning("In '", config_filepath, "', 'rounding_decimals' is missing or
+            invalid. Using default value of 2 instead.")
+      config$rounding_decimals <- 2
+    }
+
+    config
+  } else {
+
+    warning("No report configuration file provided. Using default values:
+            'current_population' = 100,000
+            'avg_5yr_population' = 100,000
+            'rounding_decimals' = 2")
+
+    config <- list(current_population = 100000,
+                   avg_5yr_population = 100000,
+                   rounding_decimals = 2)
+
+    config
+  }
+}
