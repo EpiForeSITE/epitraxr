64fdb660d7e31398301b92edce35fa2eda8f85eb
Andrew <pulsipher.a@gmail.com>
2025-07-21 16:10:33 -0600

    Update package documentation: - Add sample dataset to package under inst/sample_data/ - Add example code to README - Add script for generating sample data (not included in package) - Bump version number to 0.1.0

--- README.md
+++ README.md
@@ -23,14 +23,51 @@ You can install the development version of epitraxr from
 devtools::install_github("EpiForeSITE/epitraxr")
 ```
 
-## Example
-
-As more functions are added to the package, we will include some basic
-examples here:
+## Examples
 
 ``` r
-suppressPackageStartupMessages(
-  library(epitraxr)
+# Read the sample EpiTrax dataset included with the package
+sample_file <- system.file("sample_data", "sample_epitrax_data.csv", package = "epitraxr")
+epitrax_data <- read_epitrax_data(sample_file)
+
+# Get the list of diseases in the dataset
+diseases <- unique(epitrax_data$disease)
+
+# Set up configuration
+config <- list(
+  current_population = 102000,
+  avg_5yr_population = 97000,
+  rounding_decimals = 1
 )
-## basic example code
+
+# Generate monthly counts report for 2024
+monthly_counts <- create_report_monthly_counts(
+  data = epitrax_data,
+  y = 2024,
+  disease_names = diseases
+)
+monthly_counts
+#>      disease Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec
+#> 1 Chickenpox  26  19  22  40  16  23  37  16  21  27  14  31
+#> 2   COVID-19  81  99  84 120  89 101 125  93  80 117  99 103
+#> 3  Influenza 116  96 130 151 115 100 139 135 113 138  96 137
+#> 4    Measles  22  21  26  27  16  19  35  25  18  32  25  38
+#> 5   Syphilis  22  22  23  24  18  29  15  20  13  23  21  39
+
+# Generate YTD counts with rates per 100k (through May)
+ytd_report <- create_report_ytd_counts(
+  data = epitrax_data,
+  disease_names = diseases,
+  y = 2024,
+  m = 5,
+  config = config,
+  as.rates = TRUE
+)
+ytd_report
+#>      disease Current_YTD_Rate_per_100k Avg_5yr_YTD_Rate_per_100k
+#> 1 Chickenpox                     286.3                     107.6
+#> 2   COVID-19                    1167.6                     630.9
+#> 3  Influenza                    1437.3                     693.8
+#> 4    Measles                     298.0                     151.8
+#> 5   Syphilis                     263.7                     166.8
 ```
---
+++ scripts/generate_sample_data.R
@@ -0,0 +1,71 @@
+# ------------------------------------------------------------------------------
+# title: EpiTrax Sample Data Generator
+# author: Andrew Pulsipher
+# date: 2025-07-21
+#
+# This script generates realistic sample data for testing the EpiTrax reporting
+# package. It creates disease case data with appropriate MMWR week numbers and
+# realistic seasonal patterns for common diseases.
+#
+# The script is maintained by the Insight Net center ForeSITE. All the code is
+# available on GitHub at
+# https://github.com/EpiForeSITE/epitrax-report-automation.
+#
+# For questions, updates, or bug reports, please visit the GitHub repository.
+# ------------------------------------------------------------------------------
+
+library(lubridate)
+
+set.seed(123)  # For reproducible examples
+
+# Setup parameters
+years <- 2019:2024
+diseases <- c("COVID-19", "Influenza", "Measles", "Syphilis", "Chickenpox")
+
+# Function to get MMWR week from a date
+get_mmwr_week <- function(date) {
+  # Simple approximation - actual MMWR weeks have special rules
+  week <- week(date)
+  if (week == 53) week <- 52
+  week
+}
+
+# Generate dates and count patterns for each disease and year
+cases <- list()
+
+for (year in years) {
+  for (disease in diseases) {
+    # Determine number of cases based on disease type
+    yearly_cases <- switch(disease,
+      "COVID-19" = sample(600:2400, 1),  # 50-200 per month average
+      "Influenza" = sample(600:2400, 1),
+      "Measles" = sample(120:600, 1),    # 10-50 per month average
+      "Syphilis" = sample(120:600, 1),
+      "Chickenpox" = sample(120:360, 1)  # 10-30 per month average
+    )
+
+    # Generate random dates throughout the year
+    dates <- as.Date(paste0(year, "-01-01")) + sample(0:364, yearly_cases, replace = TRUE)
+
+    # Create cases data frame
+    year_cases <- data.frame(
+      patient_mmwr_week = sapply(dates, get_mmwr_week),
+      patient_mmwr_year = year,
+      patient_disease = disease
+    )
+
+    cases[[paste(disease, year)]] <- year_cases
+  }
+}
+
+# Combine all cases and randomize order
+epitrax_data <- do.call(rbind, cases)
+epitrax_data <- epitrax_data[sample(nrow(epitrax_data)), ]
+rownames(epitrax_data) <- NULL
+
+# Write to CSV
+utils::write.csv(
+  epitrax_data,
+  "sample_epitrax_data.csv",
+  row.names = FALSE
+)
