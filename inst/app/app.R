aa0e99903744245a26f330e2b95a4d164335af8f
Andrew Pulsipher <pulsipher.a@gmail.com>
2025-08-25 15:32:26 -0600

    Increase maximum file size to 30MB

--- inst/app/app.R
+++ inst/app/app.R
@@ -149,6 +149,8 @@ ui <- shiny::fluidPage(
 # Define server logic
 server <- function(input, output, session) {
 
+  options(shiny.maxRequestSize = 30 * 1024^2)
+
   # Reactive values to store data and reports
   epitrax_obj <- shiny::reactiveVal(NULL)
 
